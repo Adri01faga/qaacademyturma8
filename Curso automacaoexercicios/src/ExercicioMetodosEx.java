f2f1110e534642a3aa383d9c5148ff6bf7a48948
Adri01faga <adrianacristinafaga@gmail.com>
2023-04-15 15:03:22 -0300

    exercicios metodo

---
+++ Curso automacaoexercicios/src/ExercicioMetodosEx.java
@@ -0,0 +1,18 @@
+public class ExercicioMetodosEx{
+    //2- Criar 1 metodo com retorno que monte uma frase com os valores que recebeu por parâmetros
+
+    public static String texto (String primeiroTexto, String segundoTexto){
+        String resultado = primeiroTexto +segundoTexto;
+        return resultado;
+    }                     
+
+    public static void main(String[]args){
+        String primeiroTexto = "Primeiro texto leitura";
+        String segundoTexto = "Segundo texto concatena";
+        System.out.println(texto(primeiroTexto, segundoTexto));
+            }
+    }
+    //3- Criar 1 método com o retorno que some dois valores e se for maior que 100, retorne true.
+    public statis boolean maiorQueCem(int valor1, int valor2){
+        
+    }
